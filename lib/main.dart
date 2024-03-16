2220e61c9915dda14c63d79d6f776f67d16e2841
MasoudMaghsodi <magsodi333@gmail.com>
2024-03-16 22:56:23 +0330

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:fittrek/view/login-screen.dart';
 import 'package:fittrek/view/onboarding-screen.dart';
-import 'package:fittrek/view/welcome-screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
